e04668bf517d966df27bab1af88b5cb9b74ab7c3
Adarsh Kumar Jha <adarshkashyap08nov@gmail.com>
2026-07-15 15:22:39 +0530

    Initial commit

---
+++ app1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="CV BUILDER",
+    page_icon="☄️",
+    layout="Centered"
+)
+st.header("THIS IS THE APP POWERED BY GEMINI 3.5 TO YOUR CV")
